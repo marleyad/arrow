c98ed3d4bce168d2bb70730e35dce9955eae3fc1
Adam Marley <marleyad@mlc-wels.edu>
2024-01-25 22:58:27 -0700

    upload for submition

---
+++ app.js
@@ -0,0 +1,25 @@
+
+
+//originally forgot to add the const at the beginning
+//also had some trouble figuring out "= arr ="
+
+const double = arr => arr.map((val) => val * 2)
+
+//2nd challenge is a doozy
+
+// function squareAndFindEvens(numbers){
+//     var squares = numbers.map(function(num){
+//       return num ** 2;
+//     });
+//     var evens = squares.filter(function(square){
+//       return square % 2 === 0;
+//     });
+//     return evens;
+//   }
+
+
+  //My answer:
+
+  const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0)
+
+  //I learned some stuff but it was challenging. I am still getting used to the "= numbers =" format. 
